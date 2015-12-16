9f4f4476b0fac80dfd5c4b197d868fbc2a74ea31
Taras Pasichnyk <dev.taras.pasichnyk@gmail.com>
2015-12-16 23:32:14 +0200

    added login and many other changes

---
+++ PersonalTrainer/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  PersonalTrainer
+//
+//  Created by Taras Pasichnyk on 12/15/15.
+//  Copyright © 2015 Taras Pasichnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+
+@end
